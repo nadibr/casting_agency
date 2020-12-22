5d433c692675df4ac1faf8ac85469470ffa5ed26
nadibr <brusovanadez2010@students.itu.edu>
2020-12-22 19:25:35 +0200

    DB_PATH env fix for heroku

--- models.py
+++ models.py
@@ -2,14 +2,14 @@ import os
 from sqlalchemy import Column, String, Integer, DateTime, VARCHAR, create_engine
 from flask_sqlalchemy import SQLAlchemy
 
-# DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
-# DB_USER = os.getenv('DB_USER', 'postgres')
-# DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
-# DB_NAME = os.getenv('DB_NAME', 'castagency')
-#
-# DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
+DB_NAME = os.getenv('DB_NAME', 'castagency')
 
-DB_PATH = os.environ['DATABASE_URL']
+DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+
+
 
 db = SQLAlchemy()
 
