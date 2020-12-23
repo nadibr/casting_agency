00a40fa9492fa019e75c361ead300249d57b4b65
nadibr <brusovanadez2010@students.itu.edu>
2020-12-23 09:28:14 +0200

    models fix

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+# SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Enable debug mode.
+DEBUG = True
+# Database connection string
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres:password@localhost:5432/castingagency'
+# Supress warning
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- models.py
+++ models.py
@@ -19,8 +19,9 @@ db = SQLAlchemy()
 
 
 def setup_db(app, database_path=DATABASE_URL):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config.from_object('config')
+    # app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
 #    db.create_all()
